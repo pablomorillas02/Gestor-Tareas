a83a1ec572dab2d46e604e4bea4a9e558afca51f
Pablo Morillas Plaza <pmorillasplaza.pmp@gmail.com>
2023-12-10 17:53:04 +0100

    Botón de ayuda
    Se ha implementado un botón de ayuda para el subnormal que no sepa usar esta tontería.

--- app/static/js/popover.js
+++ app/static/js/popover.js
@@ -1,7 +1,6 @@
-function showPopover(){
-    var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
-        trigger: 'focus'
-      })  
-
-    return popover;
-};
+document.addEventListener('DOMContentLoaded', function () {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+});
